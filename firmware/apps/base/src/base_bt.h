1bcf82db2ab588829035186ccb918af79a885335
Sam Kwort <s.kwort@uq.net.au>
2024-05-06 10:16:11 +1000

    nrfdk: Fix typos in comments

--- firmware/apps/base/src/base_bt.h
+++ firmware/apps/base/src/base_bt.h
@@ -1,16 +1,16 @@
 #ifndef BASE_BT_H_
-#define BASE_STN_BT_H_
+#define BASE_BT_H_
 
 #include <zephyr/bluetooth/addr.h>
 #include <zephyr/kernel.h>
 
-/* Command IDs for ahu_bt cmds */
+/* Command IDs for base_bt cmds */
 #define BASE_BT_SCAN_START 0x00U
 #define BASE_BT_SCAN_STOP  0x01U
 #define BASE_BT_CONN_START 0x02U
 #define BASE_BT_CONN_STOP  0x03U
 
-/* ahu_bt_cmd_t represents commands to the ahu_bt state machine */
+/* base_bt_cmd_t represents commands to the ahu_bt state machine */
 typedef struct {
     uint8_t cmd_type;
     bool filter;
