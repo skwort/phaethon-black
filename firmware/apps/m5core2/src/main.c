2e098b11d10ec485824028da62a6f07268c32ef1
Sam Kwort <s.kwort@uq.net.au>
2024-05-17 18:08:14 +1000

    m5core2: Create DLT Link for NUS connection
    This commits sets up a DLT Link endpoint for receiving data on
    the m5core2 from the nrfdk.

--- firmware/apps/m5core2/src/main.c
+++ firmware/apps/m5core2/src/main.c
@@ -1,262 +1,67 @@
-/* main.c - Application main entry point */
-
-/*
- * Copyright (c) 2015-2016 Intel Corporation
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
-#include "zephyr/bluetooth/addr.h"
-#include "zephyr/bluetooth/services/nus.h"
-#include <string.h>
-#include <zephyr/types.h>
-#include <stddef.h>
-#include <errno.h>
+#include "zephyr/logging/log_core.h"
 #include <zephyr/kernel.h>
-#include <zephyr/sys/printk.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/uart.h>
+#include <zephyr/logging/log.h>
+#include <pb_encode.h>
+#include <pb_decode.h>
 
-#include <zephyr/bluetooth/bluetooth.h>
-#include <zephyr/bluetooth/hci.h>
-#include <zephyr/bluetooth/conn.h>
-#include <zephyr/bluetooth/uuid.h>
-#include <zephyr/bluetooth/gatt.h>
-#include <zephyr/sys/byteorder.h>
+#include "dlt_api.h"
+#include "dlt_endpoints.h"
+#include "phaethon.pb.h"
 
-/* Define the UUIDs for the GATT Attributes */
-#define BT_UUID_NUS BT_UUID_DECLARE_128(0x9E, 0xCA, 0xDC, 0x24, 0x0E, 0xE5, 0xA9, 0xE0, 0x93, 0xF3, 0xA3, 0xB5, 0x01, 0x00, 0x40, 0x6E)
-#define BT_UUID_NUS_TX BT_UUID_DECLARE_128(0x9E, 0xCA, 0xDC, 0x24, 0x0E, 0xE5, 0xA9, 0xE0, 0x93, 0xF3, 0xA3, 0xB5, 0x02, 0x00, 0x40, 0x6E)
-#define BT_UUID_NUS_RX BT_UUID_DECLARE_128(0x9E, 0xCA, 0xDC, 0x24, 0x0E, 0xE5, 0xA9, 0xE0, 0x93, 0xF3, 0xA3, 0xB5, 0x03, 0x00, 0x40, 0x6E)
-
-const char TARGET_ADDR_STR[] = "C8:91:07:19:03:58";
-
-static void start_scan(void);
-
-static struct bt_conn *default_conn;
-
-static struct bt_uuid_128 nus_uuid = BT_UUID_INIT_128(BT_UUID_NUS_SRV_VAL);
-static struct bt_uuid_16 discover_uuid;
-
-static struct bt_gatt_discover_params discover_params;
-static struct bt_gatt_subscribe_params subscribe_params;
-
-
-static uint8_t notify_func(struct bt_conn *conn,
-			   struct bt_gatt_subscribe_params *params,
-			   const void *data, uint16_t length)
-{
-	if (!data) {
-		printk("[UNSUBSCRIBED]\n");
-		params->value_handle = 0U;
-		return BT_GATT_ITER_STOP;
-	}
-
-	char s[13];
-	memcpy(s, data, length);
-	s[12] = 0;
-
-	printk("[NOTIFICATION] data %p length %u\n", data, length);
-	printk("    %s\n", s);
-
-	return BT_GATT_ITER_CONTINUE;
-}
-
-static uint8_t discover_func(struct bt_conn *conn,
-			     const struct bt_gatt_attr *attr,
-			     struct bt_gatt_discover_params *params)
-{
-	int err;
-
-	/* This branch is only triggered if attr is NULL */
-	/* i.e failed to discover the attribute defined in params */
-	if (!attr) {
-		printk("Discover complete\n");
-		/* Reset discover_params */
-		(void)memset(params, 0, sizeof(*params));
-		return BT_GATT_ITER_STOP;
-	}
-
-	printk("[ATTRIBUTE] handle %u\n", attr->handle);
-
-	/* Handle discovery of Primary Service: NUS, service0010 */
-	if (!bt_uuid_cmp(discover_params.uuid, BT_UUID_NUS)) {
-
-		/* Now try to to discover the NUS RX attribute, char0011 */	
-		memcpy(&nus_uuid, BT_UUID_NUS_RX, sizeof(nus_uuid));
-		discover_params.uuid = &nus_uuid.uuid;
-		discover_params.start_handle = attr->handle + 1;  
-		discover_params.type = BT_GATT_DISCOVER_CHARACTERISTIC;
-
-		err = bt_gatt_discover(conn, &discover_params);
-		if (err) {
-			printk("Discover failed (err %d)\n", err);
-		}
-
-	/* Handle discovery of NUS RX Service, service0010/char0011 */ 
-	} else if (!bt_uuid_cmp(discover_params.uuid, BT_UUID_NUS_RX)) {
-
-		/* Now try to discover the CCC for NUS RX */
-		memcpy(&discover_uuid, BT_UUID_GATT_CCC, sizeof(discover_uuid));
-		discover_params.uuid = &discover_uuid.uuid;
-		discover_params.start_handle = attr->handle + 2;
-		discover_params.type = BT_GATT_DISCOVER_DESCRIPTOR;
-
-		/* Value handle represents the value/char. the CCC corresponds to */
-		subscribe_params.value_handle = bt_gatt_attr_value_handle(attr);
-
-		err = bt_gatt_discover(conn, &discover_params);
-		if (err) {
-			printk("Discover failed (err %d)\n", err);
-		}
-
-	/* Handle discovery of RX service CCC, service0010/char0011/desc0013 */ 
-	} else {
-		/* Set the callback function */
-		subscribe_params.notify = notify_func;
-		subscribe_params.value = BT_GATT_CCC_NOTIFY;
-		subscribe_params.ccc_handle = attr->handle;
-
-		/* Attempt to subscribe */
-		err = bt_gatt_subscribe(conn, &subscribe_params);
-		if (err && err != -EALREADY) {
-			printk("Subscribe failed (err %d)\n", err);
-		} else {
-			printk("[SUBSCRIBED]\n");
-		}
-
-		return BT_GATT_ITER_STOP;
-	}
-
-	return BT_GATT_ITER_STOP;
-}
-
-static void device_found(const bt_addr_le_t *addr, int8_t rssi, uint8_t type,
-                         struct net_buf_simple *ad)
-{   
-    char addr_str[BT_ADDR_LE_STR_LEN];
-	int err;
-
-    if (default_conn) {
-        return;
-    }
-
-    if (type != BT_GAP_ADV_TYPE_ADV_IND && type != BT_GAP_ADV_TYPE_ADV_DIRECT_IND) {
-        return;
-    }
-
-    bt_addr_le_to_str(addr, addr_str, sizeof(addr_str));
-	printk("Device found: %s (RSSI %d)\n", addr_str, rssi);
-
-	/* Compare the MACs */
-	bt_addr_le_t target_addr;
-    err = bt_addr_le_from_str("C8:91:07:19:03:58", "random", &target_addr);
-	if (bt_addr_le_cmp(&target_addr, addr)) {
-		return;
-	} 
-
-	if (bt_le_scan_stop()) {
-		return;
-	}
-
-	err = bt_conn_le_create(addr, BT_CONN_LE_CREATE_CONN,
-				BT_LE_CONN_PARAM_DEFAULT, &default_conn);
-	if (err) {
-		printk("Create conn to %s failed (%d)\n", addr_str, err);
-		start_scan();
-	}
-
-}
-
-static void start_scan(void)
-{
-	int err;
-
-	/* Use active scanning and disable duplicate filtering to handle any
-	 * devices that might update their advertising data at runtime. */
-	struct bt_le_scan_param scan_param = {
-		.type       = BT_LE_SCAN_TYPE_ACTIVE,
-		.options    = BT_LE_SCAN_OPT_NONE,
-		.interval   = BT_GAP_SCAN_FAST_INTERVAL,
-		.window     = BT_GAP_SCAN_FAST_WINDOW,
-	};
-
-	err = bt_le_scan_start(&scan_param, device_found);
-	if (err) {
-		printk("Scanning failed to start (err %d)\n", err);
-		return;
-	}
-
-	printk("Scanning successfully started\n");
-}
-
-static void connected(struct bt_conn *conn, uint8_t conn_err)
-{
-	char addr[BT_ADDR_LE_STR_LEN];
-	int err;
-
-	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
-
-	if (conn_err) {
-		printk("Failed to connect to %s (%u)\n", addr, conn_err);
-
-		bt_conn_unref(default_conn);
-		default_conn = NULL;
-
-		start_scan();
-		return;
-	}
-
-	printk("Connected: %s\n", addr);
-
-	if (conn == default_conn) {
-		discover_params.uuid = &nus_uuid.uuid;
-		discover_params.func = discover_func;
-		discover_params.start_handle = BT_ATT_FIRST_ATTRIBUTE_HANDLE;
-		discover_params.end_handle = BT_ATT_LAST_ATTRIBUTE_HANDLE;
-		discover_params.type = BT_GATT_DISCOVER_PRIMARY;
-
-		err = bt_gatt_discover(default_conn, &discover_params);
-		if (err) {
-			printk("Discover failed(err %d)\n", err);
-			return;
-		}
-	}
-}
-
-static void disconnected(struct bt_conn *conn, uint8_t reason)
-{
-	char addr[BT_ADDR_LE_STR_LEN];
-
-	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
-
-	printk("Disconnected: %s (reason 0x%02x)\n", addr, reason);
-
-	if (default_conn != conn) {
-		return;
-	}
-
-	bt_conn_unref(default_conn);
-	default_conn = NULL;
-
-	start_scan();
-}
-
-BT_CONN_CB_DEFINE(conn_callbacks) = {
-	.connected = connected,
-	.disconnected = disconnected,
-};
+LOG_MODULE_REGISTER(m5_main, LOG_LEVEL_INF);
 
 int main(void)
 {
-	int err;
-	err = bt_enable(NULL);
+    k_tid_t device_tid = k_current_get();
+    dlt_interface_init(1);
+    dlt_device_register(device_tid);
 
-	if (err) {
-		printk("Bluetooth init failed (err %d)\n", err);
-		return 0;
-	}
+    uint8_t rx_data[DLT_MAX_DATA_LEN] = {0};
+    uint8_t resp_len = 0;
+    uint8_t msg_type = 0;
 
-	printk("Bluetooth initialized\n");
+    LOG_INF("Starting main loop");
 
-	start_scan();
-	return 0;
+    while (true) {
+
+        /* Check the PI UART Link for data */
+        resp_len = dlt_read(NRF_NUS, &msg_type, rx_data, DLT_MAX_DATA_LEN,
+                            K_NO_WAIT);
+        if (resp_len) {
+            LOG_INF("Message received.");
+
+            /* Decode message */
+            bool status;
+
+            /* Allocate space for the decoded message. */
+            ADSBData message = ADSBData_init_zero;
+
+            /* Create a stream that reads from the buffer. */
+            pb_istream_t stream = pb_istream_from_buffer(rx_data, resp_len);
+
+            /* Now we are ready to decode the message. */
+            status = pb_decode(&stream, ADSBData_fields, &message);
+
+            /* Check for errors */
+            if (status) {
+                /* Print the data contained in the message. */
+                LOG_INF("hex: %s", (char *)message.hex);
+                LOG_INF("flight: %s", message.flight);
+                LOG_INF("lat: %f", (double)message.lat);
+                LOG_INF("lon: %f", (double)message.lon);
+                LOG_INF("alt: %d", message.altitude);
+                LOG_INF("speed: %d", message.speed);
+                LOG_INF("track: %d", message.track);
+            } else {
+                LOG_ERR("Decoding failed: %s\n", PB_GET_ERROR(&stream));
+            }
+
+        }
+
+        k_sleep(K_MSEC(3));
+    }
+
+    return 0;
 }
